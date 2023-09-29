ce29822bd841ffafdb8a65144258e2a1548c2a8f
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-29 15:53:46 +0200

    feat: create file readme.md and index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- php -->
+<?php ?>
+
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>PHP Object-oriented programming 1</title>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Gabriele Di Emmanuele" />
+    <meta name="description" content="php-oop-1" />
+</head>
+
+<body>
+</body>
+
+</html>
