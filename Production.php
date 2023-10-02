13f4042f3229f8594f666968eb8e808f3b34a2d4
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-02 15:43:28 +0200

    feat: add Production code

--- Production.php
+++ Production.php
@@ -9,24 +9,26 @@ class Production
 
     public $year;
 
-    //* con __construct costruisco le stringhe dei miei film (come prova).
     public function __construct(
-        //* affermo che sono stringe
         string $title,
         Genre $genre,
         string $year
     ) {
-        //*li trasformo in stringhe
         $this->title = $title;
         $this->genre = $genre;
         $this->year = $year;
     }
 
-    //* tramite metodo, creo la stringa della descrizione completa del film.
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
     public function getDescription()
     {
         return $this->title . " " . $this->genre->genre . " " . $this->year;
     }
+
 
 }
 ?>
